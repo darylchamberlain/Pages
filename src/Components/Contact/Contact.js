0ca805109469e7dad8bd44e4cb150fc606b43275
Dary <darylbills@yahoo.com>
2020-07-03 16:31:40 -0400

    removed MDBContainer

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Contact.css'
 import NavBar from '../Body/NavBar/NavBar'
 import FormPage from './FormPage'
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
+// import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
 
 class Contact extends React.Component{
     render(){
